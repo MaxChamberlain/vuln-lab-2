0b58df5cb35b4b207c3b4807818f72aab5060e94
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2023-02-14 16:33:47 -0700

    Add regex to validate email address before proceeding

--- src/CSC245_Project2.java
+++ src/CSC245_Project2.java
@@ -1,22 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CSC245_Project2 {
 
     /*  DISCOVERED VULNERABILITIES
      * 1. Does not check email address for script tags
-     *
+     * solved
      * 2. Does not check email address for unescaped "illegal" characters
      *      ( # ! = @ % $ . )
-     *
+     * solved
      * 3. Does not check validity of domain carrier
      *      (.com => .comnfg)
-     *
+     * solved
      * 4. Does not check validity of domain host
      *      (@arapahoe.edu => @..edu)
-     *
-     * 5. Does not check length of Email name (ed@...)
+     * solved
+     * 5. Does not check length of Email name
+     *      (ed@...)
+     * solved
      */
 
     public static void main(String[] args) {
@@ -42,7 +46,19 @@ public class CSC245_Project2 {
                  * I feel like there should be more checks here...
                  */
                 while ((fileLine = inputStream.readLine()) != null) {
-                    System.out.println(fileLine);
+                    boolean valid = false;
+                    // Regex for email address
+                    String regex = "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+                    Pattern pattern = Pattern.compile(regex);
+                    Matcher matcher = pattern.matcher(fileLine);
+                    if (matcher.matches()) {
+                        valid = true;
+                    }
+                    if (valid) {
+                        System.out.println(fileLine);
+                    } else {
+                        System.out.println("Invalid Email Address: " + fileLine);
+                    }
                 }
             } catch (IOException io) {
                 System.out.println("File IO exception" + io.getMessage());
