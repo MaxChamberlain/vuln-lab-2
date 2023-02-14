7ee801eedcd92d8cbd124a72e4cb26201b38fff7
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2023-02-14 16:45:16 -0700

    replace try/catch/finally with try-with-resources/catch to close the inputstream under all circumstances.

--- src/CSC245_Project2.java
+++ src/CSC245_Project2.java
@@ -8,75 +8,57 @@ public class CSC245_Project2 {
 
     /*  DISCOVERED VULNERABILITIES
      * 1. Does not check email address for script tags
-     * solved
+     * solved with regex validation
      * 2. Does not check email address for unescaped "illegal" characters
      *      ( # ! = @ % $ . )
-     * solved
+     * solved with regex validation
      * 3. Does not check validity of domain carrier
      *      (.com => .comnfg)
-     * solved
+     * solved with regex validation
      * 4. Does not check validity of domain host
      *      (@arapahoe.edu => @..edu)
-     * solved
+     * solved with regex validation
      * 5. Does not check length of Email name
      *      (ed@...)
-     * solved
+     * solved with regex validation
+     *
+     * 6. Filename is never checked for validity (null, empty, traversal, etc.)
+     * !!!
+     *
+     * 7. Input stream is never closed if there is an error (memory leak)
+     * solved by using try-with-resources instead of try-finally
      */
 
     public static void main(String[] args) {
-        // Read the filename from the command line argument
         String filename = args[0];
-        BufferedReader inputStream = null;
-        /*
-         * filename is not sanitized
-         */
         String fileLine;
-        try {
-            try {
-                /*
-                 * Shouldn't this be in its own try/catch?
-                 */
-                inputStream = new BufferedReader(new FileReader(filename));
+        // WHY ARE THERE TWO TRY/CATCH BLOCKS? I removed one but why was it there in the first place?
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(filename))) {            /*
+             * Shouldn't this be in its own try/catch?
+             */
 
-                System.out.println("Email Addresses:");
+            System.out.println("Email Addresses:");
 
-                // Read one Line using BufferedReader
-
-                /*
-                 * I feel like there should be more checks here...
-                 */
-                while ((fileLine = inputStream.readLine()) != null) {
-                    boolean valid = false;
-                    // Regex for email address
-                    String regex = "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-                    Pattern pattern = Pattern.compile(regex);
-                    Matcher matcher = pattern.matcher(fileLine);
-                    if (matcher.matches()) {
-                        valid = true;
-                    }
-                    if (valid) {
-                        System.out.println(fileLine);
-                    } else {
-                        System.out.println("Invalid Email Address: " + fileLine);
-                    }
+            /*
+             * I feel like there should be more checks here...
+             */
+            while ((fileLine = inputStream.readLine()) != null) {
+                boolean valid = false;
+                // Regex for email address
+                String regex = "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(fileLine);
+                if (matcher.matches()) {
+                    valid = true;
                 }
-            } catch (IOException io) {
-                System.out.println("File IO exception" + io.getMessage());
-            } finally {
-                // Need another catch for closing
-                // the streams
-                try {
-                    if (inputStream != null) {
-                        inputStream.close();
-                    }
-                } catch (IOException io) {
-                    System.out.println("Issue closing the Files" + io.getMessage());
+                if (valid) {
+                    System.out.println(fileLine);
+                } else {
+                    System.out.println("Invalid Email Address: " + fileLine);
                 }
-
             }
-        } catch (Error e) {
-            System.out.println(e);
+        } catch (IOException io) {
+            System.out.println("File IO exception" + io.getMessage());
         }
-
     }
 }
