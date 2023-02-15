9518498b60a041acd286716d83b53ec08d7161cb
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2023-02-14 17:22:04 -0700

    add regex validation for filename on init

--- src/CSC245_Project2.java
+++ src/CSC245_Project2.java
@@ -8,46 +8,51 @@ public class CSC245_Project2 {
 
     /*  DISCOVERED VULNERABILITIES
      * 1. Does not check email address for script tags
-     * solved with regex validation
+     *      solved with regex validation
+     *
      * 2. Does not check email address for unescaped "illegal" characters
      *      ( # ! = @ % $ . )
-     * solved with regex validation
+     *          solved with regex validation
+     *
      * 3. Does not check validity of domain carrier
      *      (.com => .comnfg)
-     * solved with regex validation
+     *          solved with regex validation
+     *
      * 4. Does not check validity of domain host
      *      (@arapahoe.edu => @..edu)
-     * solved with regex validation
+     *          solved with regex validation
+     *
      * 5. Does not check length of Email name
      *      (ed@...)
-     * solved with regex validation
+     *          solved with regex validation
      *
      * 6. Filename is never checked for validity (null, empty, traversal, etc.)
-     * !!!
+     *          solved with regex to check filename validity
      *
      * 7. Input stream is never closed if there is an error (memory leak)
-     * solved by using try-with-resources instead of try-finally
+     *          solved by using try-with-resources instead of try-catch-finally
      */
 
     public static void main(String[] args) {
+
+        // Check for filename
         String filename = args[0];
+        if (filename == null || filename.isEmpty()) throw new IllegalArgumentException("No filename");
+        String regex = "^[a-zA-Z0-9_]+\\.[a-zA-Z]+$"; // Regex for valid characters
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(filename);
+        if(!matcher.matches()) throw new IllegalArgumentException("Invalid filename");
+
         String fileLine;
         // WHY ARE THERE TWO TRY/CATCH BLOCKS? I removed one but why was it there in the first place?
-        try (BufferedReader inputStream = new BufferedReader(new FileReader(filename))) {            /*
-             * Shouldn't this be in its own try/catch?
-             */
-
+        try (BufferedReader inputStream = new BufferedReader(new FileReader(filename))) {   // try-with-resources
             System.out.println("Email Addresses:");
-
-            /*
-             * I feel like there should be more checks here...
-             */
             while ((fileLine = inputStream.readLine()) != null) {
                 boolean valid = false;
                 // Regex for email address
-                String regex = "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(fileLine);
+                regex = "^[a-zA-Z0-9_.+-]{3,}@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]{2,}$";
+                pattern = Pattern.compile(regex);
+                matcher = pattern.matcher(fileLine);
                 if (matcher.matches()) {
                     valid = true;
                 }
